1de9b35fbff32d7614ff9068bc5f0b18535577e6
yusikerem <kerem.yky@gmail.com>
2024-05-24 14:22:39 +0300

    add user to store

--- client/src/app/Context/store.tsx
+++ client/src/app/Context/store.tsx
@@ -12,6 +12,22 @@ import {
 interface ContextProps {
   activeModal: string;
   setActiveModal: Dispatch<SetStateAction<string>>;
+  setUser: Dispatch<
+    SetStateAction<{
+      userName: string;
+      prefferedName: string;
+      genres: string[];
+      likes: string[];
+      avatar: string;
+    }>
+  >;
+  user: {
+    userName: string;
+    prefferedName: string;
+    genres: string[];
+    likes: string[];
+    avatar: string;
+  };
 }
 type RootContentProps = {
   children: ReactNode;
@@ -19,13 +35,30 @@ type RootContentProps = {
 
 const GlobalContext = createContext<ContextProps>({
   activeModal: "",
+  user: {
+    userName: "",
+    prefferedName: "",
+    genres: [],
+    likes: [],
+    avatar: "",
+  },
+  setUser: (): string => "",
   setActiveModal: (): string => "",
 });
 export const GlobalContextProvider = ({ children }: RootContentProps) => {
   const [activeModal, setActiveModal] = useState("");
+  const [user, setUser] = useState<ContextProps["user"]>({
+    userName: "",
+    prefferedName: "",
+    genres: [],
+    likes: [],
+    avatar: "",
+  });
 
   return (
-    <GlobalContext.Provider value={{ activeModal, setActiveModal }}>
+    <GlobalContext.Provider
+      value={{ activeModal, setActiveModal, user, setUser }}
+    >
       {children}
     </GlobalContext.Provider>
   );
