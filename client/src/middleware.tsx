00c9385c5a8d37684843fab52d4f6cebd8ad1a53
yusikerem <kerem.yky@gmail.com>
2024-06-04 16:56:11 +0300

    add middleware to prevent acces some pages from unauthorized users

---
+++ client/src/middleware.tsx
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+    matcher:[ '/user','/liked','/rated'],
+  }
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("jwtToken");
+
+  if (!token) {
+    return NextResponse.redirect("http://localhost:3000/");
+  }
+
+  return NextResponse.next();
+}
